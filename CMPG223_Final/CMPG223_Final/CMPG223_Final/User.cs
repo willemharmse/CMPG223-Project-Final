730d42bb1bb715298a7f395b9f04f803a93297e9
willemharmse <harmse688@gmail.com>
2023-08-31 08:22:07 +0200

    User class to contain user info

---
+++ CMPG223_Final/CMPG223_Final/CMPG223_Final/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMPG223_Final
+{
+    public static class User
+    {
+        private static String username;
+        private static String password;
+        private static Boolean isAdmin;
+        private static int id;
+    }
+}
