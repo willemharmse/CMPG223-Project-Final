9dcd29e246f3b9ec826231fce5714167adb7ac48
ShaldonSenekal <shaldon60@gmail.com>
2023-09-04 14:35:36 +0200

    Payment Panel, and Maintain panel gui changes

--- CMPG223_Final/CMPG223_Final/CMPG223_Final/Dashboard.Designer.cs
+++ CMPG223_Final/CMPG223_Final/CMPG223_Final/Dashboard.Designer.cs
@@ -78,11 +78,27 @@
             this.lblMaintainData = new System.Windows.Forms.Label();
             this.btnLogOut = new System.Windows.Forms.Button();
             this.btnHelp = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUsernameSU = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.btnProcessPay = new System.Windows.Forms.Button();
             this.pnlQuotes.SuspendLayout();
+            this.pnlPayments.SuspendLayout();
             this.pnlReports.SuspendLayout();
             this.gbReports.SuspendLayout();
             this.pnlMaintain.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblTitle
@@ -363,6 +379,18 @@
             // pnlPayments
             // 
             this.pnlPayments.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.pnlPayments.Controls.Add(this.btnProcessPay);
+            this.pnlPayments.Controls.Add(this.textBox2);
+            this.pnlPayments.Controls.Add(this.label11);
+            this.pnlPayments.Controls.Add(this.label12);
+            this.pnlPayments.Controls.Add(this.textBox1);
+            this.pnlPayments.Controls.Add(this.label8);
+            this.pnlPayments.Controls.Add(this.label9);
+            this.pnlPayments.Controls.Add(this.txtUsernameSU);
+            this.pnlPayments.Controls.Add(this.label6);
+            this.pnlPayments.Controls.Add(this.label7);
+            this.pnlPayments.Controls.Add(this.label5);
+            this.pnlPayments.Controls.Add(this.label4);
             this.pnlPayments.Location = new System.Drawing.Point(71, 102);
             this.pnlPayments.Name = "pnlPayments";
             this.pnlPayments.Size = new System.Drawing.Size(949, 486);
@@ -511,6 +539,8 @@
             // pnlMaintain
             // 
             this.pnlMaintain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.pnlMaintain.Controls.Add(this.numericUpDown1);
+            this.pnlMaintain.Controls.Add(this.label2);
             this.pnlMaintain.Controls.Add(this.txtSearch);
             this.pnlMaintain.Controls.Add(this.lblSUUsername);
             this.pnlMaintain.Controls.Add(this.label3);
@@ -546,9 +576,9 @@
             this.lblSUUsername.ForeColor = System.Drawing.Color.White;
             this.lblSUUsername.Location = new System.Drawing.Point(44, 249);
             this.lblSUUsername.Name = "lblSUUsername";
-            this.lblSUUsername.Size = new System.Drawing.Size(54, 15);
+            this.lblSUUsername.Size = new System.Drawing.Size(158, 15);
             this.lblSUUsername.TabIndex = 59;
-            this.lblSUUsername.Text = "Search";
+            this.lblSUUsername.Text = "General Search by Text";
             // 
             // label3
             // 
@@ -710,6 +740,164 @@
             this.btnHelp.Text = "Help";
             this.btnHelp.UseVisualStyleBackColor = false;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(43, 309);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 15);
+            this.label2.TabIndex = 61;
+            this.label2.Text = "General Search by Number";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.numericUpDown1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F);
+            this.numericUpDown1.ForeColor = System.Drawing.Color.White;
+            this.numericUpDown1.Location = new System.Drawing.Point(46, 330);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(247, 23);
+            this.numericUpDown1.TabIndex = 62;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(307, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(345, 40);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Payment of Service";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(214, 471);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(532, 12);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Your banking details will not be saved by the system. It will only be used for th" +
+    "is one transaction";
+            // 
+            // txtUsernameSU
+            // 
+            this.txtUsernameSU.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.txtUsernameSU.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsernameSU.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsernameSU.ForeColor = System.Drawing.Color.White;
+            this.txtUsernameSU.Location = new System.Drawing.Point(370, 108);
+            this.txtUsernameSU.Name = "txtUsernameSU";
+            this.txtUsernameSU.Size = new System.Drawing.Size(230, 24);
+            this.txtUsernameSU.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(367, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(139, 25);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Card Number";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(367, 114);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(246, 25);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "                                       ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(370, 185);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 24);
+            this.textBox1.TabIndex = 21;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(367, 157);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 25);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "CVV";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(367, 192);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(246, 25);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "                                       ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(370, 265);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(230, 24);
+            this.textBox2.TabIndex = 24;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(367, 233);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(159, 25);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Expiration Date";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.White;
+            this.label12.Location = new System.Drawing.Point(367, 272);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(246, 25);
+            this.label12.TabIndex = 26;
+            this.label12.Text = "                                       ";
+            // 
+            // btnProcessPay
+            // 
+            this.btnProcessPay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.btnProcessPay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProcessPay.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcessPay.ForeColor = System.Drawing.Color.White;
+            this.btnProcessPay.Location = new System.Drawing.Point(370, 348);
+            this.btnProcessPay.Name = "btnProcessPay";
+            this.btnProcessPay.Size = new System.Drawing.Size(243, 64);
+            this.btnProcessPay.TabIndex = 55;
+            this.btnProcessPay.Text = "Process Payment";
+            this.btnProcessPay.UseVisualStyleBackColor = false;
+            // 
             // frmDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -725,10 +913,10 @@
             this.Controls.Add(this.lblQuotes);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.pnlMaintain);
-            this.Controls.Add(this.pnlReports);
             this.Controls.Add(this.pnlPayments);
             this.Controls.Add(this.pnlQuotes);
+            this.Controls.Add(this.pnlMaintain);
+            this.Controls.Add(this.pnlReports);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmDashboard";
@@ -736,6 +924,8 @@
             this.Load += new System.EventHandler(this.frmDashboard_Load);
             this.pnlQuotes.ResumeLayout(false);
             this.pnlQuotes.PerformLayout();
+            this.pnlPayments.ResumeLayout(false);
+            this.pnlPayments.PerformLayout();
             this.pnlReports.ResumeLayout(false);
             this.pnlReports.PerformLayout();
             this.gbReports.ResumeLayout(false);
@@ -743,6 +933,7 @@
             this.pnlMaintain.ResumeLayout(false);
             this.pnlMaintain.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -795,5 +986,19 @@
         private System.Windows.Forms.TextBox txtSearch;
         private System.Windows.Forms.Label lblSUUsername;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnProcessPay;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtUsernameSU;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
